37202b28c5dc5f65dc6eba5498a47d56653b7d07
ramarfx <ramagamiing33@gmail.com>
2023-12-14 21:07:40 +0700

    order by roles

--- app/Http/Controllers/admin/UsersController.php
+++ app/Http/Controllers/admin/UsersController.php
@@ -6,12 +6,27 @@ use App\Http\Controllers\Controller;
 use App\Models\Role;
 use Illuminate\Http\Request;
 use App\Models\User;
+use Ramsey\Uuid\Guid\Fields;
 
 class UsersController extends Controller
 {
     public function index()
     {
-        $users = User::with('roles')->get();
+        //ambil seluruh user dan urutkan berdasarkan role
+        $users = User::join('role_user', 'users.id', '=', 'role_user.user_id')
+            ->join('roles', 'role_user.role_id', '=', 'roles.id')
+            ->orderByRaw("
+            CASE
+                WHEN roles.name = 'admin' THEN 1
+                WHEN roles.name = 'teacher' THEN 2
+                WHEN roles.name = 'student_council' THEN 3
+                WHEN roles.name = 'ekskul' THEN 4
+                WHEN roles.name = 'student' THEN 5
+                ELSE 6
+            END
+        ")
+        ->with('roles')
+        ->get();
         $roles = Role::all();
 
         return view('admin.account.index', compact('users', 'roles'));
@@ -19,8 +34,11 @@ class UsersController extends Controller
 
     public function update(Request $request)
     {
-        $user = User::find($request->user_id);
-        $user->roles()->sync([$request->role_id]);
+        $userId = $request->input('user_id');
+        $roleId = $request->input('role_id');
+
+        $user = User::find($userId);
+        $user->roles()->sync([$roleId]);
 
         return back();
     }
